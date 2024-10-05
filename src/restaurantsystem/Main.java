7003e3dcc8bb5f1313754b913e5877a34b8dfa46
saro <shanmugamj735@gmail.com>
2024-10-05 21:26:13 +0530

    COMPLETE

---
+++ src/restaurantsystem/Main.java
@@ -0,0 +1,58 @@
+
+package restaurantsystem;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import restaurantsystem.component.auth.Login;
+
+/**
+ *
+ * @author Shanmugam
+ */
+public class Main extends JFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        createRequiredFileIfDoesNotExist();
+        
+        Login im = new Login();
+        im.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        im.setVisible(true);
+    }
+    
+    private static void createRequiredFileIfDoesNotExist() {
+        String fileNames [];
+        
+        File rootDir = new File("storage");
+        rootDir.mkdirs();
+        
+        fileNames = new String [] {"storage/item.txt",
+            "storage/labour.txt",
+            "storage/order.txt",
+            "storage/orderLine.txt"};
+        
+        for (String fileName : fileNames) {
+            File file = new File(fileName);
+            if(!file.exists())
+            {  
+                try {
+                    file.createNewFile();
+                } catch (IOException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        
+       
+    }
+
+}
